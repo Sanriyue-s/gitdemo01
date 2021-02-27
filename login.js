6aaefd0fbedc33124832904fc4e4eb4ea28a4b32
三日月 <1607530511@qq.com>
2021-02-27 19:43:09 +0800

    xin

---
+++ login.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.fr').on('click',function(){
+        $('.form').hide()
+        $('.reg-box').show()
+     
+    })
+    $('.dl').on('click',function(){
+        $('.form').show()
+        $('.reg-box').hide()
+     
+    })
+})
