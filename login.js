bed484553ea76cc9686ec64a28bae80eca4897d8
三日月 <1607530511@qq.com>
2021-02-28 14:47:38 +0800

    zhuche

--- login.js
+++ login.js
@@ -9,4 +9,11 @@ $(function(){
         $('.reg-box').hide()
      
     })
+    var form = layui.form
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ] 
+    })
 })
